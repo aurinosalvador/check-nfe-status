210cfda0e612ff1352159753b5be8e8292aa9bc3
Aurino Salvador <aurinosalvador@gmail.com>
2021-10-14 08:07:55 -0300

    Primeiro Commit

---
+++ src/main/java/io/github/aurinosalvador/checknfestatus/CheckNfeStatusApplication.java
@@ -0,0 +1,13 @@
+package io.github.aurinosalvador.checknfestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckNfeStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckNfeStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aurinosalvador/checknfestatus/CheckNfeStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aurinosalvador.checknfestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckNfeStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

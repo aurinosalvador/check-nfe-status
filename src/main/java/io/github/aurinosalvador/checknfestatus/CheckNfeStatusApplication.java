90589eb8ec0f9b893616b6496acb7ab58b3214b9
Aurino Salvador <aurinosalvador@gmail.com>
2021-10-14 08:19:21 -0300

    Implementação de anotações de scan na classe principal

--- src/main/java/io/github/aurinosalvador/checknfestatus/CheckNfeStatusApplication.java
+++ src/main/java/io/github/aurinosalvador/checknfestatus/CheckNfeStatusApplication.java
@@ -2,8 +2,10 @@ package io.github.aurinosalvador.checknfestatus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.github.aurinosalvador")
+@EntityScan(basePackages = "io.github.aurinosalvador.entities")
 public class CheckNfeStatusApplication {
 
 	public static void main(String[] args) {
